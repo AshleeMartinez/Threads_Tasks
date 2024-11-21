d5aae8fe137c08d725670955e2a26fb5f2d508d2
AshleeMartinez <praviaashlee2@gmail.com>
2024-11-20 21:34:00 -0600

    Hilos y tareas

---
+++ Tasks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tareas_Hilos
+{
+    public  class Tasks
+    {
+        static async Task  Main()
+        {
+            Task task1 = Task.Run(() =>
+            {
+                Task.Delay(2000).Wait(); // Simula trabajo.
+                Console.WriteLine("Tarea 1 completada.");
+            });
+
+            Task task2 = Task.Run(() =>
+            {
+                Task.Delay(1000).Wait(); // Simula trabajo.
+                Console.WriteLine("Tarea 2 completada.");
+            });
+
+            await Task.WhenAny(task1, task2); // Espera a que una termine.
+            Console.WriteLine("Al menos una tarea ha terminado.");
+
+
+        }
+    }
+}
---
+++ Threads.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Tareas_Hilos
+{
+    public class Threads
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo de Threads (Hilos)
+            // Creacion de hilos
+            Thread Hilo_Numeros = new Thread(ImprimirNumeros);
+            Thread Hilo_Letras = new Thread(ImprimirLetras);
+
+            //Inicia ejecución
+            Hilo_Numeros.Start();
+            Hilo_Letras.Start();
+
+            // Esto hace que el subproceso principal espere hasta que numberThread y letterThread hayan completado su ejecución antes de continuar.
+            Hilo_Numeros.Join();
+            Hilo_Letras.Join();
+
+            Console.WriteLine("Ambos hilos completados");
+
+        }
+
+        static void ImprimirNumeros()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Numero: {i}");
+                Thread.Sleep(500); // Simulate work
+            }
+        }
+        static void ImprimirLetras()
+        {
+            for (char c = 'A'; c <= 'E'; c++)
+            {
+                Console.WriteLine($"Numero: {c}");
+                Thread.Sleep(700); // Simulate work
+            }
+        }
+
+
+    }
+}
